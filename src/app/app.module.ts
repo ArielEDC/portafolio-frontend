af0a2511cb428f983d52a11572973569a84e020e
ariel <adiazcarreno@gmail.com>
2022-04-20 10:30:57 -0300

    maquetado inicial en Angular, portafolio web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { MainComponent } from './componentes/main/main.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { SobreMiComponent } from './componentes/main/sobre-mi/sobre-mi.component';
+import { ExperienciaComponent } from './componentes/main/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/main/educacion/educacion.component';
+import { HabilidadesComponent } from './componentes/main/habilidades/habilidades.component';
+import { ProyectosComponent } from './componentes/main/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    MainComponent,
+    FooterComponent,
+    SobreMiComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HabilidadesComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule
